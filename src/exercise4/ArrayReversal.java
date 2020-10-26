f0f1ed08d08004bef8495b147f3adb248f748658
Thrace123 <72183930+Thrace123@users.noreply.github.com>
2020-10-26 22:30:13 +0000

    5

---
+++ src/exercise4/ArrayReversal.java
@@ -0,0 +1,29 @@
+package exercise4;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArrayReversal {
+  
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int Grosse = input.nextInt();
+    char [] smth = new char[Grosse];
+    
+    for (int index = 0; index < smth.length; index++) {
+      preslava[i] = input.next().charAt(0);
+    }
+
+    char [] smth2 = new char[Grosse];
+    int index = smth.length - 1;
+    int numb = 0;
+    for (;index >= 0; index--){
+      smth1[numb] = smth1[i];
+      address++;
+    }
+
+    String charToString = Arrays.toString(smth1);
+    System.out.println("Reversed symbols: " + charToString);
+  }
+}
